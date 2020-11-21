fff30400e10afc6b2db6aaa9142f97453f47202b
Cody Bennett <hi@codyb.co>
2020-11-21 08:32:38 -0600

    Create input components.

---
+++ src/components/Icon/index.js
@@ -0,0 +1,17 @@
+import classNames from 'classnames';
+import { ReactComponent as Error } from 'assets/icons/error.svg';
+import './index.css';
+
+export const icons = {
+  error: Error,
+};
+
+const Icon = ({ icon, style, className, ...rest }) => {
+  const IconComponent = icons[icon];
+
+  return (
+    <IconComponent aria-hidden className={classNames('icon', className)} {...rest} />
+  );
+};
+
+export default Icon;

684420e2a8f0575c51c320c11a9b4cdb34541893
Cody Bennett <hi@codyb.co>
2021-03-14 00:46:23 -0600

    Inline OCR dataset.

--- src/ocr/index.js
+++ src/ocr/index.js
@@ -1,12 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import { createWorker, PSM, ImageLike } from 'tesseract.js';
+import { requireAtRuntime } from 'utils/electron';
+
+const { join } = requireAtRuntime('path');
 
 /**
  * Evaluates an image context, returning multi-line text
  * @param {ImageLike} image Image context to evaluate and decode
  */
 export const evaluateImage = async image => {
-  const worker = createWorker();
+  const worker = createWorker({ langPath: join(__dirname, '../../public') });
 
   await worker.load();
   await worker.loadLanguage('eng');
